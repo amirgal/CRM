315df76a50e3122ca755932318270d5494d38ead
amirgal <amirc538@gmail.com>
2020-03-26 14:14:32 +0200

    actions update client - all fields

--- src/components/ActionsComponents/UpdateClient.js
+++ src/components/ActionsComponents/UpdateClient.js
@@ -3,8 +3,6 @@ import { inject, observer } from 'mobx-react';
 import {List, ListItem, Button, TextField, Checkbox} from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import {CheckBox as CheckBoxIcon, CheckBoxOutlineBlank as CheckBoxOutlineBlankIcon} from '@material-ui/icons'
-// import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-// import CheckBoxIcon from '@material-ui/icons/CheckBox';
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
@@ -56,7 +54,35 @@ class UpdateClient extends React.Component{
                             )}
                         />
                     </ListItem>
-                   
+                    <ListItem>
+                        <div className="update-client-row" id="update-owner">
+                            <Autocomplete
+                                id="select-owner"
+                                options={[{name:'elliot'},{name:'perkins'}]}
+                                getOptionLabel={option => option.name}
+                                style={{ width: 300 }}
+                                renderInput={params => <TextField {...params}
+                                    label="Select Owner"/>}
+                            />
+                            <Button color='primary' variant='outlined'>Transfer Ownership</Button>
+                        </div>
+                    </ListItem>
+                    <ListItem>
+                        <div className="update-client-row">
+                            <Autocomplete
+                                id="select-email-type"
+                                options={[{type:'A'},{type:'B'},{type:'C'},{type:'D'}]}
+                                getOptionLabel={option => option.type}
+                                style={{ width: 300 }}
+                                renderInput={params => <TextField {...params}
+                                    label="Select Email To Send"/>}
+                            />
+                            <Button color='primary' variant='outlined'>Send Email</Button>
+                        </div>
+                    </ListItem>
+                    <ListItem>
+                        <Button color='primary' variant='contained'>Declare Sale!</Button>
+                    </ListItem>
                 </List>
             </form>
             </div>
